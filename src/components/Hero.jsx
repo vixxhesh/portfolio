00dc006273295d71c0f1037c32a92a23790fbffb
Vishesh Verma <vishesh.verma1@s.amity.edu>
2023-04-11 23:10:02 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../style';
+import { ComputersCanvas } from './canvas';
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#9152ff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Vishesh</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop websites and mobile applications <br className='sm:block hidden'/> using MERN stack
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
