fa4d91b0f6c67f59666245b812aa1e5ab15c030a
Vishesh Verma <80572483+vixxhesh@users.noreply.github.com>
2023-04-14 11:29:45 +0530

    made changes for text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+       I'm a proficient software developer with extensive experience in HTML, CSS, JavaScript, and Bootstrap, along with specialized knowledge in React, Node.js, and MongoDB. Additionally, I have expertise in AI/ML projects focused on image and object detection. My aptitude for rapid learning and close collaboration with clients enables me to produce efficient, scalable, and user-friendly solutions that address real-world problems. I'm eager to work with you and bring your ideas to fruition!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
