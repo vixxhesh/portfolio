b324a8420af00dda1d3577e533a6ea3e306070b3
vixxhesh <vishesh.verma1@s.amity.edu>
2024-12-13 21:24:48 +0530

    Update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../style";
@@ -7,7 +7,6 @@ import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
-
 
 const ProjectCard = ({
   index,
@@ -19,44 +18,41 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      
       <Tilt
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className='bg-dd-green p-5 rounded-2xl sm:w-[360px] w-full'
-        
+        className="bg-dd-green p-5 rounded-2xl sm:w-[360px] w-full"
       >
-      
-        <div className='relative w-full h-[230px]'>
+        <div className="relative w-full h-[230px]">
           <img
             src={image}
-            alt='project_image'
-            className='w-full h-full object-contain rounded-2xl'
+            alt="project_image"
+            className="w-full h-full object-contain rounded-2xl"
           />
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
                 src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
               />
             </div>
           </div>
         </div>
 
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
+        <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
@@ -79,24 +75,29 @@ const Works = () => {
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
-      
+      <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          I'm a proficient software developer with extensive experience in HTML, CSS, JavaScript, and Bootstrap, along with specialized knowledge in React, Node.js, and MongoDB. Additionally, I have expertise in mobile app development using Flutter and React Native, and I'm proficient in MySQL for SQL databases. I have a strong understanding of hosting and deployment, as well as integrating systems through REST APIs. With my aptitude for rapid learning and close collaboration with clients, I can produce efficient, scalable, and user-friendly solutions that address real-world problems. I'm eager to work with you and bring your ideas to fruition!
+          I'm a proficient software developer with extensive experience in HTML,
+          CSS, JavaScript, and Bootstrap, along with specialized knowledge in
+          React, Node.js, and MongoDB. Additionally, I have expertise in mobile
+          app development using Flutter and React Native, and I'm proficient in
+          MySQL for SQL databases. I have a strong understanding of hosting and
+          deployment, as well as integrating systems through REST APIs. With my
+          aptitude for rapid learning and close collaboration with clients, I
+          can produce efficient, scalable, and user-friendly solutions that
+          address real-world problems. I'm eager to work with you and bring your
+          ideas to fruition!
         </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
-        
+      <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
-
         ))}
       </div>
-      
     </>
   );
 };
